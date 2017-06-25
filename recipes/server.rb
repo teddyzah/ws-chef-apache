9d519b2c2145b7eaf5c7f6aedf5e84724eefdbba
teddyzah <teddyzah@yahoo.com>
2017-06-25 14:58:06 +0000

    first commit

---
+++ recipes/server.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: apache
+# Recipe:: server
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
+
+package 'httpd' do
+  action :install
+end
+
+file '/var/www/html/index.html' do
+  content '<h1>Hello Tedwin</h1>'
+end
+
+service 'httpd' do
+  action [:enable, :start]
+end

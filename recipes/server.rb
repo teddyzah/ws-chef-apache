ccfce8b86c4154731e706dd95b3b3a40619947f1
teddyzah <teddyzah@yahoo.com>
2017-06-25 18:13:24 +0000

    template created for index.html

--- recipes/server.rb
+++ recipes/server.rb
@@ -8,10 +8,9 @@ package 'httpd' do
   action :install
 end
 
-file '/var/www/html/index.html' do
-  content "<h1>Hello Tedwin</h1>
-  <h2>IPADDRESS = #{node['ipaddress']}</h2>
-  <h2>HOSTNAME = #{node['hostname']}</h2>"
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+  action :create
 end
 
 service 'httpd' do
